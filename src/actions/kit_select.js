67dfa1a883b0ff12060d5e6c4761d31fd096fb8b
Mathew Docker <mathew.docker@gmail.com>
2018-11-11 08:51:21 +1100

    merge changes

--- src/actions/kit_select.js
+++ src/actions/kit_select.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export function SelectKit(kit) {
   console.log(`KitSelect Called: ${kit}`);
   // SelectKit is an AntionCreator it needs to return an
@@ -8,14 +7,3 @@ export function SelectKit(kit) {
     payload: kit
   };
 }
-=======
-export function SelectKit(kit) {
-  console.log(`KitSelect Called: ${kit}`);
-  // SelectKit is an AntionCreator it needs to return an
-  // action, an object with a type property
-  return {
-    type: "KIT_SELECTED",
-    payload: kit
-  };
-}
->>>>>>> f6179564ebf740428ec352f839758aeff8d243ea
