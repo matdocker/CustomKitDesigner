8553a826e84ad40615d43ea07df52e0325ec1f27
Mathew Docker <38601846+matdocker@users.noreply.github.com>
2018-11-12 20:08:27 +1100

    updates

--- src/actions/build_select.js
+++ src/actions/build_select.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export function SelectMenu(buildMenu) {
   console.log(`KitSelect Called: ${buildMenu}`);
   // SelectMenu is an AntionCreator it needs to return an
@@ -8,14 +7,3 @@ export function SelectMenu(buildMenu) {
     payload: buildMenu
   };
 }
-=======
-export function SelectMenu(buildMenu) {
-  console.log(`KitSelect Called: ${buildMenu}`);
-  // SelectMenu is an AntionCreator it needs to return an
-  // action, an object with a type property
-  return {
-    type: "B_MENU_SELECTED",
-    payload: buildMenu
-  };
-}
->>>>>>> f6179564ebf740428ec352f839758aeff8d243ea
--- src/actions/logo_selected.js
+++ src/actions/logo_selected.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export function logoSelected(url) {
   console.log(`LogoURLSelected Called: ${url}`);
   // designSelected is an AntionCreator it needs to return an
@@ -8,14 +7,3 @@ export function logoSelected(url) {
     payload: url
   };
 }
-=======
-export function desingSelected(design) {
-  console.log(`desingSelected Called: ${design}`);
-  // designSelected is an AntionCreator it needs to return an
-  // action, an object with a type property
-  return {
-    type: "DESIGN_SELECTED",
-    payload: design
-  };
-}
->>>>>>> f6179564ebf740428ec352f839758aeff8d243ea
